b1f9b7523f16ecc00d5ae383e444c0c1867562bf
root <root@unknown>
2014-05-13 13:43:00 +0100

    aazaza

---
+++ app/cache/dev/twig/27/cc/b0c4ecd3f11c1ee511f2066ab1261868df2bd6867ee506404445657e95b9.php
@@ -0,0 +1,49 @@
+<?php
+
+/* VoyageBundle:Default:stat.html.twig */
+class __TwigTemplate_27ccb0c4ecd3f11c1ee511f2066ab1261868df2bd6867ee506404445657e95b9 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("VoyageBundle::layout.html.twig");
+
+        $this->blocks = array(
+            'contains' => array($this, 'block_contains'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "VoyageBundle::layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_contains($context, array $blocks = array())
+    {
+        // line 4
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "VoyageBundle:Default:stat.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 4,  28 => 3,);
+    }
+}
